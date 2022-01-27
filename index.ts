c73be48cf141f8ed011f551342b35499aaa68f32
sha1n <sha1n@users.noreply.github.com>
2022-01-27 18:21:03 +0200

    exported missing function

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { TimeUnit, toMilliseconds } from './lib/timeunit';
-export { sleep, delay, stopwatch, until, eventually } from './lib/utilities';
+export { withTimeout, sleep, delay, stopwatch, until, eventually } from './lib/utilities';
 export {
   RetryPolicy,
   retryAround,
